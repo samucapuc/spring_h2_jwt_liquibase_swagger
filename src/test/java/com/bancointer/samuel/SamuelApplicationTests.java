3a9c602375e617cfcf7c81439bc3a4091a21e331
samucapuc <samucapuc@gmail.com>
2019-05-21 21:14:04 -0300

    Created Project

---
+++ src/test/java/com/bancointer/samuel/SamuelApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bancointer.samuel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SamuelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+
+}
